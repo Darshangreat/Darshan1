6d415eac911d9cfbf832f95ba949539489a3063d
Darshan <darshanparui7@gmail.com>
2025-08-13 14:25:16 +0530

    Add files via upload

---
+++ Hello World/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+int main() {
+    int num;
+
+
+
+    printf("ENTER A NUMBER: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d", num, factorial (num));
+
+    return 0;
+}
